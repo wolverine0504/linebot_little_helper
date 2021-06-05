fc1df4fa8477892deea6e11f717ac3f82085a576
wolverine0504 <heng61308@gmail.com>
2021-06-06 02:59:02 +0800

    make it better

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, abort
+
+
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (
+    MessageEvent,
+    TextMessage,
+    TextSendMessage,
+    TemplateSendMessage,
+    QuickReply,
+    QuickReplyButton,
+    MessageAction,
+    PostbackAction, ImagemapSendMessage, ImageSendMessage, StickerSendMessage, AudioSendMessage, LocationSendMessage,
+    FlexSendMessage, VideoSendMessage,
+)
+from linebot.models.events import JoinEvent, PostbackEvent,MemberJoinedEvent,MemberLeftEvent
+
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('3dl/t0sL0uKA2/vt0fACm5FgMnP6Ba4bVE89258SDraqlkq355fff824TVW3WvIHVSEYCWbhv6A38oNcOue/R8u2orOreInHietxvPlg3SzjbQIbK/osq3xQc4QHLDI0hHXaV5NcWSWHR/wXeCOpSwdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('b7982d4e833c3efb4a5ac372b52b441d')
+
+
+
+def getMessageObject(jsonObject):
+    message_type = jsonObject.get('type')
+    if message_type == 'text':
+        return TextSendMessage.new_from_json_dict(jsonObject)
+    elif message_type == 'imagemap':
+        return ImagemapSendMessage.new_from_json_dict(jsonObject)
+    elif message_type == 'template':
+        return TemplateSendMessage.new_from_json_dict(jsonObject)
+    elif message_type == 'image':
+        return ImageSendMessage.new_from_json_dict(jsonObject)
+    elif message_type == 'sticker':
+        return StickerSendMessage.new_from_json_dict(jsonObject)
+    elif message_type == 'audio':
+        return AudioSendMessage.new_from_json_dict(jsonObject)
+    elif message_type == 'location':
+        return LocationSendMessage.new_from_json_dict(jsonObject)
+    elif message_type == 'flex':
+        return FlexSendMessage.new_from_json_dict(jsonObject)
+    elif message_type == 'video':
+        return VideoSendMessage.new_from_json_dict(jsonObject)
+
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="我是誰是臥底小幫手"))
+
+
+if __name__ == "__main__":
+     app.run()
